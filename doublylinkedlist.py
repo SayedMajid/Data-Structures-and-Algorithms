a275ebe260a325cdbee04273b8b8fcb4623a5639
Majid Sayyed <sydmajid04@gmail.com>
2023-02-22 20:38:47 +0530

    Deleting a node from DLL

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -41,11 +41,54 @@ class DoublyLinkedList:
             print(current_node.value)
             current_node = current_node.prev
 
+    def delete_node(self, value):
+        current_node = self.head
+
+        # ~# Search for the value
+        # * Start from head, if not found go to current.next and stop when its None(null)
+
+        while current_node is not None:
+            if current_node.value is value:
+                # ~! Laavaris node xD
+                if current_node is self.head and current_node is self.tail:
+                    print("Removing the only node", current_node.value)
+                    self.head = None
+                    self.tail = None
+                    return True
+
+                # ~! Checking if its the first node
+                if current_node is self.head:
+                    print("Removing the first node", current_node.value)
+                    self.head = self.head.next
+                    self.head.prev = None
+                    return True
+
+                # ~! Checking if its the last node
+                if current_node is self.tail:
+                    print("Removing the tail node", current_node.value)
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+                    return True
+
+                # ~! Deleting node anywhere from the middle of DLL
+                print("Removing", current_node.value)
+                current_node.prev.next = current_node.next
+                current_node.next.prev = current_node.prev
+
+            current_node = current_node.next
+
 
 dll = DoublyLinkedList()
 dll.add_node(5)
 dll.add_node(7)
+dll.add_node(9)
 dll.add_node(1)
 dll.print_forward()
 print("-" * 10)
 dll.print_backward()
+
+dll.delete_node(1)
+dll.delete_node(5)
+dll.delete_node(9)
+dll.delete_node(7)
+dll.print_forward()
